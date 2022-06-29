b1dd9fe8df719e65282de1239b48b978be3e8159
Amanda Amabili Beirao Honorio <aamabili@BR0C02F74F0MD6M.local>
2022-06-29 13:28:32 -0300

    Creat addList

--- src/com/company/CategoriaCircuito.java
+++ src/com/company/CategoriaCircuito.java
@@ -4,8 +4,8 @@ public interface CategoriaCircuito {
 
     int maioridade = 18;
 
-    void verificarCircuito(String inscricao, int idade, String nome);
-    void pagamentoCircuitoPequeno(int idade);
-    void pagamentoCircuitoMedio(int idade);
-    void pagamentoCircuitoAvancado(int idade,  String nome);
+    void verificarCircuito(String inscricao, int idade, String nome, Participante participante);
+    void pagamentoCircuitoPequeno(int idade, Participante participante);
+    void pagamentoCircuitoMedio(int idade, Participante participante);
+    void pagamentoCircuitoAvancado(int idade,  String nome, Participante participante);
     }
