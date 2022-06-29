4594b466b3c26356fcaed01e65580f7ff19dca07
Amanda Amabili Beirao Honorio <aamabili@BR0C02F74F0MD6M.local>
2022-06-29 13:28:32 -0300

    Creat addList

--- src/com/company/InscricaoCircuito.java
+++ src/com/company/InscricaoCircuito.java
@@ -1,93 +1,99 @@
 package com.company;
 
-import java.util.InputMismatchException;
-import java.util.List;
+import java.util.*;
 
 public class InscricaoCircuito  implements CategoriaCircuito, Inscricao  {
-    private Participante participante;
-    private List<Participante> ppList = List.of();
-    private List<Participante> pmList = List.of();
-    private List<Participante> pgList = List.of();
+//    private Participante participante;
+    private  List<Participante> ppList = new ArrayList<Participante>();
+    private List<Participante> pmList = new ArrayList<Participante>();
+    private List<Participante> pgList = new ArrayList<Participante>();
 
 
-    public InscricaoCircuito(Participante participante) {
-        this.participante = participante;
-        System.out.println(participante.inscricao );
-    }
-
-
-    public void realizarInscricao(Participante participante){
-        //validar qual o tipo de inscrição
-        ppList.add(participante);
-
+    public InscricaoCircuito() {
     }
 
     public boolean verificarMaiorIdade(int idade) {
         return idade > maioridade;
     }
 
-    public void realizarInscricao(){
-
+    public void realizarInscricao(Participante participante){
         try {
             System.out.println("Ola " + participante.nome + participante.sobrenome + " !!");
-            this.verificarCircuito(participante.inscricao, participante.idade, participante.nome);
-        } catch (InputMismatchException e){
+            System.out.println(participante);
+
+            this.verificarCircuito(participante.inscricao, participante.idade, participante.nome, participante);
+             } catch (InputMismatchException e){
             System.out.println(e.getMessage() );
         }
 
     }
 
-    public void verificarCircuito(String inscricao, int idade, String nome){
+    public void verificarCircuito(String inscricao, int idade, String nome, Participante participante){
         try{
             if (inscricao.equals("circuito_pequeno") ) {
-                this.pagamentoCircuitoPequeno(idade);
+                this.pagamentoCircuitoPequeno(idade, participante);
+                return;
             }
             if (inscricao.equals("circuito_medio")) {
-                this.pagamentoCircuitoMedio(idade);
+                this.pagamentoCircuitoMedio(idade, participante);
+                return;
             }
             if (inscricao.equals("circuito_avancado")) {
-                this.pagamentoCircuitoAvancado(idade,nome );
+                this.pagamentoCircuitoAvancado(idade,nome, participante );
+                return;
             }
-        }catch (Exception e){
+             }catch (Exception e){
             System.out.println(e.getMessage() );
-
         }
 
 
     }
 
-    public void pagamentoCircuitoPequeno(int idade){
+
+
+
+    public void pagamentoCircuitoPequeno(int idade, Participante participante){
         try{
             boolean verifyMaiority = this.verificarMaiorIdade(idade);
-            if (verifyMaiority){
+        ppList.add(participante);
+
+        if (verifyMaiority){
                 System.out.println("voce vai pagar R$1.500.");
             }else {
+
                 System.out.println("Voce vai pagar R$1.300.");
             }
+
+        System.out.println(ppList.size());
+
+
         }catch (Exception e) {
             System.out.println(e.getMessage() );
         }
 
     }
 
-    public void pagamentoCircuitoMedio(int idade){
+    public void pagamentoCircuitoMedio(int idade, Participante participante){
         try{
             boolean verifyMaiority = this.verificarMaiorIdade(idade);
+            pmList.add(participante);
 
             if (verifyMaiority){
                 System.out.println("voce vai pagar R$2.300.");
             }else {
                 System.out.println("Voce vai pagar R$2.000");
             }
-        }catch (Exception e) {
+
+             }catch (Exception e) {
             System.out.println(e.getMessage() );
         }
 
     }
 
-    public void pagamentoCircuitoAvancado(int idade,  String nome) throws Error {
+    public void pagamentoCircuitoAvancado(int idade,  String nome, Participante participante) throws Error {
         try {
             boolean verifyMaiority = this.verificarMaiorIdade(idade);
+            pgList.add(participante);
 
             if (verifyMaiority){
                 System.out.println("voce vai pagar R$2.800.");
@@ -95,9 +101,15 @@ public class InscricaoCircuito  implements CategoriaCircuito, Inscricao  {
             }else {
                 throw new Error( nome + " Por ser menor de idade não tem autorizacao para participar nessa categoria");
             }
-        }catch (Exception e){
+
+             }catch (Exception e){
             System.out.println(e.getMessage() );
         }
+
+    }
+
+    @Override
+    public void realizarInscricao() {
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,17 +12,17 @@ public class Main {
         Participante p1= new Participante( "5.863.817", "Amanda Amabili", "Beirao Honorio", 27,"48998316108", "48998316109", "O+", "circuito_pequeno");
         Participante p2= new Participante("5.863.817", "Rossini", "Beirao Honorio", 27,"48998316108", "48998316109", "O+", "circuito_avancado");
 
-        InscricaoCircuito in = new InscricaoCircuito(p1);
-//        participantsTotals.add(p1);
-//        participantsTotals.add(p2);
+        InscricaoCircuito inscricaop1 = new InscricaoCircuito();
+        InscricaoCircuito inscricaop2 = new InscricaoCircuito();
 
-       participantsTotals.stream()
-               .filter(participante -> participante.getInscricao().equals("circuito_pequeno"))
-               .collect(Collectors.toList())
-               .forEach(System.out::println);
 
-       in.realizarInscricao();
-        p2.realizarInscricao();
+//        participantsTotals.stream()
+//               .filter(participante -> participante.getInscricao().equals("circuito_pequeno"))
+//               .collect(Collectors.toList())
+//               .forEach(System.out::println);
+
+        inscricaop1.realizarInscricao(p1);
+//        inscricaop2.realizarInscricao(p2);
 
 
     }
